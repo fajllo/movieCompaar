daba4b0fcbb95f8887830acb081245375d97995c
fsdr <filipsordyl8@unilever.com>
2020-09-05 21:59:29 +0200

    full movie data

--- app.js
+++ app.js
@@ -2,14 +2,28 @@
 
 // request to http://www.omdbapi.com/?apikey=[15915ed8]&
 
-async function fetchData(){
+async function searchMovie(){
     const response = await axios.get("http://www.omdbapi.com/",{
         params: {
             apikey:"15915ed8",
             s: "Avengers"
         }
     });
-    console.log(response)
+    console.log(response.data)
+
 }
 
-fetchData()
+searchMovie()
+
+
+async function fullMovie(){
+    const response = await axios.get("http://www.omdbapi.com/",{
+        params: {
+            apikey:"15915ed8",
+            i: "tt0848228"
+        }
+    });
+    console.log(response.data)
+}
+
+fullMovie()
