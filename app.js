5a76afe48ac0d50dc9279d792231f340aaa84deb
fsdr <filipsordyl8@unilever.com>
2020-09-06 20:37:49 +0200

    html in js

--- app.js
+++ app.js
@@ -3,6 +3,21 @@
 // request to http://www.omdbapi.com/?apikey=[15915ed8]&
 
 
+// old html <div class="dropdown is-active">
+    //       <input type="text" name="" id="">
+    //       <div class="dropdown-menu">
+    //         <div class="dropdown-content">
+    //           <a href="" class="dropdown-item">Mocie #1</a>
+    //           <a href="" class="dropdown-item">Mocie #1</a>
+    //           <a href="" class="dropdown-item">Mocie #1</a>
+
+    //         </div>
+    //       </div>
+    //   </div>  
+
+
+
+
 async function searchMovie(searchFraze){
     const response = await axios.get("http://www.omdbapi.com/",{
         params: {
@@ -29,7 +44,18 @@ async function fullMovie(){
 
 
 
+
+const autocomplete = document.querySelector(".autocomplete");
+autocomplete.innerHTML = `
+<label for=""><b>Search for a movie!</b></label>    
+<input type="text" name="" id="">
+<div class="dropdown ">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+</div>`
 const input = document.querySelector("input");
+
 const onInput =  async event => {
     const movies = await searchMovie(event.target.value)
     for(let movie of movies){
