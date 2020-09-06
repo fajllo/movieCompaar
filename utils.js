836b32a4966c63c335025a268de33b4044bd080b
fsdr <filipsordyl8@unilever.com>
2020-09-06 19:40:23 +0200

    move debounce function in to new file

---
+++ utils.js
@@ -0,0 +1,11 @@
+function debounce(callback){
+    let timeoutId;
+    return (...args) => {
+        if(timeoutId){
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            callback.apply(null,args);
+        },800);
+    };
+};
